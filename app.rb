a85275ebb1cd0aa880186aa96e4d2eaad6ed300d
fenjiot <fenjiot@gmail.com>
2014-09-24 14:16:44 -0400

    Updated to make links work.
    Updated Pixtr files to link it all up.

--- app.rb
+++ app.rb
@@ -16,25 +16,31 @@ database = PG.connect({ dbname: "photo_gallery" })
 # gem install sinatra-contrib <- to use sinatra/reloader development?
 # Handle an HTTP GET request to '/'
 get "/" do
-  galleries = database.exec_params("SELECT name FROM galleries")
-  @gallery_names = galleries.map do |gallery|
-    gallery["name"]
-  end
-  # Alt way to do above: @gallery_names = galleries.map { |gallery|
-  # gallery["name"] }
-  puts @gallery_names.inspect
+  galleries = database.exec_params("SELECT id, name FROM galleries")
+  @gallery_ids_names = galleries.map { |gallery| gallery }
   erb :home
 end
 
-get "/gallery/:name" do
-  @name = params[:name]
-  @images = GALLERIES[@name]
+get "/gallery/:id" do
+  id = params[:id]
+  # Getting name from database
+  query_name = "SELECT name FROM galleries WHERE id = $1"
+  name_result = database.exec_params(query_name, [id])
+  @name = name_result.first["name"]
+
+  # Getting image urls from database 
+  query_images = "SELECT url FROM images WHERE gallery_id = $1"
+
+  images_result = database.exec_params(query_images, [id])
+  @images = images_result.map { |image| image["url"] }
+  puts images_result.inspect
+  puts @images.inspect
   erb :gallery
 end
 
-get "/projects/:id" do
-  @project_id = params["id"]
-  # note: :id is also acceptable above. @project_id = params[:id]
-  erb :project
-end
+# get "/projects/:id" do
+#   @project_id = params["id"]
+#   # note: :id is also acceptable above. @project_id = params[:id]
+#   erb :project
+#end
 
