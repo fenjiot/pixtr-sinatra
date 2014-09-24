34c41f40245ec990c7a9611a59093741ab5edd03
fenjiot <fenjiot@gmail.com>
2014-09-24 16:26:11 -0400

    Mainly changed gallery.erb to galleries.erb
    Changed 'gallery' to 'galleries' because of naming conventions that will presist
    into rails.  A way to look at it is that we are calling all galleries then a
    specific gallery from that. So, galleries/cats

--- app.rb
+++ app.rb
@@ -1,11 +1,5 @@
 require "sinatra"
 require "sinatra/reloader" if development?
-# START Alt way to require sinatra/reloader
-# if development?
-#   require "sinatra/reloader"
-# end
-# END Alt way
-
 require "active_record"
 
 ActiveRecord::Base.establish_connection(
@@ -26,10 +20,14 @@ get "/" do
   erb :home
 end
 
-get "/gallery/:id" do
+get "/galleries/new" do
+  erb :new_gallery
+end
+
+get "/galleries/:id" do
   id = params[:id]
   @gallery = Gallery.find(id)
   @images = Image.where(gallery_id: id)
-  erb :gallery
+  erb :galleries
 end
 
